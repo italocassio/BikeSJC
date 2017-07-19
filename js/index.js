f7ec2c953c78d6ce3296a1a5500858da29b834d1
Ítalo Cássio <italocassio95@gmail.com>
2017-07-18 22:57:37 -0300

    login e cadastro de usuario
    novos ajustes

--- js/index.js
+++ js/index.js
@@ -23,90 +23,90 @@ var app = {
 
 var login = "";
 var latlong = "";
+
 function acessar(div) {
-    $(".divPage").hide();
+  $(".divPage").hide();
 	if (div != 'divMaster'){
 		$("#mapaestacoes").hide();
 	} else {
 		$('#hfmapa').click();
-		
 		$('ul.tabs').tabs('select_tab', 'hfmapa');
 		$("#mapaestacoes").show();
 	}
 	
 	$("#btnVoltarEstacao").hide();
-    $("#" + div).show();        
+  $("#" + div).show();        
 }
 
 function entrar(div) {
 	$(".divPage").hide();
-    $("#divLogin").hide();
+  $("#divLogin").hide();
 	$("#btnVoltarEstacao").hide();
-    $("#mapaestacoes").show();
-    $("#" + div).show();
-    $("#hfmapa").click();
+  $("#mapaestacoes").show();
+  $("#" + div).show();
+  $("#hfmapa").click();
 
-    var sjc = {lat: -23.1899556, lng: -45.86557};
+  var sjc = {lat: -23.1899556, lng: -45.86557};
        
-    var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
         center: sjc
-      });
+     });
 
-
+    //resgata a localização do usuário
     navigator.geolocation.getCurrentPosition(function(position) {
-                                    
-
-                latlong = {lat: position.coords.latitude, lng: position.coords.longitude};
+              
+        latlong = {lat: position.coords.latitude, lng: position.coords.longitude};
                                 
-                var marker = new google.maps.Marker({
-                  position: latlong,
-                  map: map
-                });
+        var marker = new google.maps.Marker({
+          position: latlong,
+          map: map
+        });
 
-                map.setCenter(latlong); //centraliza o mapa na localização atual
+        map.setCenter(latlong); //centraliza o mapa na localização atual
 
-                var features = [
-                  {
-                    position: new google.maps.LatLng(-23.1999782, -45.8907102),
-                    type: 'parking',
-                    latlng: "-23.1999782, -45.8907102"
-                  }, {
-                    position: new google.maps.LatLng(-23.1954351, -45.900689),
-                    type: 'parking',
-                    latlng: "-23.1954351, -45.900689"
-                  }
-                ];
+        //registra as localizações das estações
+        var features = [
+          {
+            position: new google.maps.LatLng(-23.1999782, -45.8907102),
+            type: 'parking',
+            latlng: "-23.1999782, -45.8907102"
+          }, {
+            position: new google.maps.LatLng(-23.1954351, -45.900689),
+            type: 'parking',
+            latlng: "-23.1954351, -45.900689"
+          }
+        ];
 
-
-        // Create markers.
+        // Cria os marcadores com base nas localizações das estações.
         features.forEach(function(feature) {
           var marker = new google.maps.Marker({
             position: feature.position,
             map: map,
             icon: 'img/estation.png'
           });
+          // ao clicar no marcador, exibe os detalhes da estação
           marker.addListener('click', function() {
             infoEstacao(feature.latlng);
-        });
-
+          });
         });
                 
-                }, function(error) {
-                           if(error.code == PositionError.PERMISSION_DENIED) {
-                                      alert("Sem permissão para utilizar o GPS!");
-                           } else if(error.code == PositionError.POSITION_UNAVAILABLE) {
-                                      alert("GPS não encontrado!");
-                           } else if(error.code == PositionError.TIMEOUT) {
-                                      alert("Está demorando para obter localização!");
-                           } else {
-                                      alert("Erro desconhecido no GPS!");
-                           }
-                }, { enableHighAccuracy: true, maximumAge: 9999999 });
+    }, function(error) {
+           if(error.code == PositionError.PERMISSION_DENIED) {
+                      alert("Sem permissão para utilizar o GPS!");
+           } else if(error.code == PositionError.POSITION_UNAVAILABLE) {
+                      alert("GPS não encontrado!");
+           } else if(error.code == PositionError.TIMEOUT) {
+                      alert("Está demorando para obter localização!");
+           } else {
+                      alert("Erro desconhecido no GPS!");
+           }
+    }, { enableHighAccuracy: true, maximumAge: 9999999 });
+
+          window.localStorage.setItem('login','teste');
     
-    window.localStorage.setItem('login','teste');
-    
-}
+    }
+
 //cria botao ver rotas
 function infoEstacao (latlng) {
    $('#modal1').modal('open');
@@ -145,25 +145,170 @@ function verRota(lat,long) {
 }
 
 function alugarBike() {
-	swal({
-  title: "Deseja alugar esta bike?",
-  text: "Será descontado 1 passe diário.",
-  type: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#DD6B55",
-  confirmButtonText: "Sim",
-  cancelButtonText: "Não",
-  closeOnConfirm: false,
-  closeOnCancel: false
-},
-function(isConfirm){
-  if (isConfirm) {
-    swal("Sucesso!", "Sua bike está disponível!", "success");
-  } else {
-    swal("Cancelado!", "Ei.. vamos dá uma volta de bike!", "error");
-  }
-});
+    	swal({
+      title: "Deseja alugar esta bike?",
+      text: "Será descontado 1 passe diário.",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Sim",
+      cancelButtonText: "Não",
+      closeOnConfirm: false,
+      closeOnCancel: false
+    },
+    function(isConfirm){
+      if (isConfirm) {
+        swal("Sucesso!", "Sua bike está disponível!", "success");
+      } else {
+        swal("Cancelado!", "Ei.. vamos dá uma volta de bike!", "error");
+      }
+    });
 }
+
+function AcessarLogin () {
+  
+  if($("#txtLogin").val() == "") {
+       swal("Atenção!", "Preenche o campo login.", "warning");
+       return false;
+  }
+
+  if($("#txtSenha").val() == "") {
+       swal("Atenção!", "Preenche o campo senha.", "warning");
+       return false;
+  }
+
+  $("#loader").show();
+
+  var objDados = {
+    usuario: $("#txtLogin").val() , 
+    senha: $("#txtSenha").val()
+  }
+  $.ajax({
+          type: "POST",
+          url: "http://bikesjc.azurewebsites.net/login",
+          contentType: "application/json; charset=utf-8",
+          dataType: "json",
+          data: JSON.stringify(objDados),
+          success: function (response) {
+            $("#loader").hide();
+            var usuario = response;
+            
+            if(usuario.nome == null) {
+                swal('Atenção!', 'login ou senha incorretos.', 'error')
+            } else {
+              $("#spNome").html("Olá, " + usuario.nome);
+              Materialize.toast('Seja bem vindo ' + usuario.nome, 4000);
+              entrar('divMaster');
+            }
+            
+          },
+          failure: function (response) {
+            $("#loader").hide();
+            alert(response.d);
+          },
+          error: function (response) {
+            $("#loader").hide();
+            alert(response.d);
+          }
+  });
+  
+}
+
+function CadastrarUsuario () {
+  
+  if($("#txtnome").val() == "") {
+       swal("Atenção!", "Preenche o campo nome.", "warning");
+       return false;
+  }
+
+  if($("#txtcpf").val() == "") {
+       swal("Atenção!", "Preenche o campo cpf.", "warning");
+       return false;
+  }
+
+  if($("#txtemail").val() == "") {
+       swal("Atenção!", "Preenche o campo email.", "warning");
+       return false;
+  }
+
+  if($("#txttelefone").val() == "") {
+       swal("Atenção!", "Preenche o campo telefone.", "warning");
+       return false;
+  }
+
+  if($("#txtCadLogin").val() == "") {
+       swal("Atenção!", "Preenche o campo login.", "warning");
+       return false;
+  }
+
+  if($("#txtCadSenha").val() == "") {
+       swal("Atenção!", "Preenche o campo senha.", "warning");
+       return false;
+  }
+
+  if($("#txtCadCSenha").val() == "") {
+       swal("Atenção!", "Preenche o campo confirmação da senha.", "warning");
+       return false;
+  }
+
+  if($("#txtCadSenha").val() != $("#txtCadCSenha").val()) {
+       swal("Atenção!", "As senhas são diferentes.", "warning");
+       return false;
+  }
+
+  $("#loader").show();
+
+  var objDados = {
+    nome: $("#txtnome").val(),
+    cpf: $("#txtcpf").val(), 
+    email: $("#txtemail").val(),
+    telefone: $("#txttelefone").val(),
+    login: $("#txtCadLogin").val(),
+    senha: $("#txtCadSenha").val()
+  }
+
+  $.ajax({
+          type: "POST",
+          url: "http://bikesjc.azurewebsites.net/cadastro/usuario",
+          contentType: "application/json; charset=utf-8",
+          dataType: "json",
+          data: JSON.stringify(objDados),
+          success: function (response) {
+            $("#loader").hide();
+           
+            if(response) {
+                swal({
+                    title: "Sucesso!",
+                    text: "Pronto agora você pode acessar o app.",
+                    type: "success",
+                    showCancelButton: false,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Entrar agora",
+                    closeOnConfirm: false
+                  },
+                  function(){                    
+                      $(".divPage").hide();
+                      $("#divLogin").show();
+                  });
+               
+            } else {
+              swal('Ops!', 'Não foi possível realizar cadastro.', 'error')
+            }
+            
+          },
+          failure: function (response) {
+            $("#loader").hide();
+            alert(response.d);
+          },
+          error: function (response) {
+            $("#loader").hide();
+            alert(response.d);
+          }
+  });
+  
+}
+
+
 
 $(function() {
   $("#divLogin").show();
@@ -175,7 +320,7 @@ $(function() {
     }
   );
   $('.modal').modal();
- app.initialize();
+  app.initialize();
 
 });
 
